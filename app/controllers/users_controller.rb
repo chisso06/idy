01ef38e16e19619994c3e063294c10b1e6b0a26d
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-02-21 23:12:07 +0900

    create signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "idyへようこそ！"
-      redirect_to root_path
+      redirect_to root_url
     else
       flash[:notice] = "ログインに失敗しました"
       render "new"
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get new_user_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         user_name: "",
+                                         email: "",
+                                         password: "",
+                                         password_confirmation: "" } }
+    end
+    assert_template "users/new"
+    assert_not flash.empty?
+  end
+
+  test "valid signup information" do
+    get new_user_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "user",
+                                         user_name: "user",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template "/"
+    assert_not flash.empty?
+  end
+end
