fa05e08fe1330f9dda7f29d8da833a1f2c5dada6
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-02-23 12:30:05 +0900

    do user-controller-test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,35 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:hanako)
+  end
+
+  test "should get new" do
+    get new_user_path
+    assert_response :success
+  end
+
+  test "should get login_form" do
+    get login_path
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_path(@user)
+    assert_response :success
+  end
+
+  test "should get show" do
+    get user_path(@user)
+    assert_response :success
+  end
+
+  test "should get index" do
+    get users_path
+    assert_response :success
+  end
+
+
 end
