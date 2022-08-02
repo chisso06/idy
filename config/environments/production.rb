0563840dad721eee3f418780e57be23cac7758fe
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-08-02 22:01:55 +0900

    host setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'idy-new.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     port: 587,
     address: ADDRESS,
