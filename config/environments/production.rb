ca9d2871d628d0e67bba51a1a1880b0d144a263c
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-08-03 09:04:06 +0900

    mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 require "active_support/core_ext/integer/time"
 ADDRESS   = 'smtp.gmail.com'
 DOMAIN    = 'gmail.com'
-PASSWORD  = 'qyirlaolwscfaggx'
-USER_NAME = 'milia4364@gmail.com'
+PASSWORD  = 'cjiitqgosypkjvcj'
+USER_NAME = 'idy.newidea@gmail.com'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
