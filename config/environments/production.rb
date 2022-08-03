018bf0e3012550d9a54b85c3c7a9605c344c9f0f
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-08-04 03:19:49 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,10 @@
 require "active_support/core_ext/integer/time"
 
+ADDRESS   = 'smtp.gmail.com'
+DOMAIN    = 'gmail.com'
+PASSWORD  = 'kikzhvxmqyathldf'
+USER_NAME = 'idy.newidea@gmail.com'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
